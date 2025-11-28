128bbe7d31e2792342f338c2fb9aaa5f5f78dec9
Gabriel Leal <2006gabrielleal@gmail.com>
2025-11-27 21:49:59 -0300

    Feito card de candidata com variações de props (para diferentes fluxos)

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -16,6 +16,7 @@ import { SecretariaLoginForm } from '@/components/core/secretaria-login-form';
 import { UserRegistrationForm } from '@/components/core/usuaria-registration-form';
 import { useAuth } from "@/contexts/AuthContext";
 import { PublishJobForm } from '@/components/core/publish-job-form';
+import { CandidateCard } from '@/components/core/candidate-card';
 
 export default function SandboxPage() {
   const { user, logout } = useAuth();
@@ -27,7 +28,7 @@ export default function SandboxPage() {
   return (
     <div>
       <Header onLogout={() => handleLogout()} />
-      
+
       {/* <SearchBar />
       <JobCard
         jobType="Tipo de vaga"
@@ -71,7 +72,7 @@ export default function SandboxPage() {
 
       {/* <SecretariaRegistrationForm /> */}
 
-      <CompanyLoginForm />  
+      <CompanyLoginForm />
       <SecretariaLoginForm />
 
       <UserRegistrationForm />
@@ -79,6 +80,34 @@ export default function SandboxPage() {
 
       <PublishJobForm />
 
+      <div className="p-8 space-y-4 max-w-2xl mx-auto">
+
+        <CandidateCard
+          // propriedades orbigatórias
+          id="cand-001"
+          name="Clara Menezes"
+          role="Desenvolvedora Front-end Pleno"
+          location="São Paulo, SP"
+          onViewProfile={() => console.log("Usuário clicou em Ver Perfil")}
+
+          // propriedas opocionais (dependendo da view do fluxo)
+          matchPercentage={85}
+          imageUrl="https://github.com/shadcn.png"
+          status="interview"
+          onApprove={() => console.log("Candidato APROVADO!")}
+          onReject={() => console.log("Candidato REJEITADO!")}
+        />
+
+        <CandidateCard
+          id="cand-002"
+          name="Luana Guimarães"
+          role="Analista de RH"
+          location="Rio de Janeiro, RJ"
+          matchPercentage={62}
+          onViewProfile={() => console.log("Usuário clicou em Ver Perfil")}
+        // Exemplo sem os botões de aprovar/rejeitar e sem foto
+        />
+      </div>
     </div>
   );
 }
